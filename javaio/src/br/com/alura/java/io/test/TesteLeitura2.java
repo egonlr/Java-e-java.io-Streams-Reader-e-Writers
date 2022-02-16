fd15ef650c9ca0dfb7ca63948467296b3a5c2b0e
egonlr <egonlr@hotmail.com>
2022-02-16 13:21:19 -0300

    Parseando arquivo

--- javaio/src/br/com/alura/java/io/test/TesteLeitura2.java
+++ javaio/src/br/com/alura/java/io/test/TesteLeitura2.java
@@ -1,6 +1,8 @@
 package br.com.alura.java.io.test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -14,8 +16,24 @@ public class TesteLeitura2 {
 		String linha = sc.nextLine();
 		System.out.println(linha);
 		
+		Scanner linhasc = new Scanner(linha);
+		linhasc.useLocale(Locale.US);
+		linhasc.useDelimiter(",");
+		
+		String valor1 = linhasc.next();
+		int valor2 = linhasc.nextInt();
+		int valor3 = linhasc.nextInt();
+		String valor4 = linhasc.next();
+		double valor5 = linhasc.nextDouble();
+		
+		System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+		linhasc.close();
+		//String [] valores = linha.split(",");
+		//System.out.println(Arrays.toString(valores));
+		//System.out.println(valores[3]);
+		
 	}
-	
+
 	sc.close();
 	
 	}
